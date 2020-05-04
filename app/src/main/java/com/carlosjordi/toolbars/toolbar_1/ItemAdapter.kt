313d6755b89458df39e7286004400e29bbdf5b7c
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2020-05-04 04:06:28 -0500

    Adapter para el layout del item listo

--- app/src/main/java/com/carlosjordi/toolbars/toolbar_1/ItemAdapter.kt
+++ app/src/main/java/com/carlosjordi/toolbars/toolbar_1/ItemAdapter.kt
@@ -1,5 +1,6 @@
 package com.carlosjordi.toolbars.toolbar_1
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -8,18 +9,34 @@ import com.carlosjordi.toolbars.databinding.ItemLayoutBinding
 
 class ItemAdapter : ListAdapter<Item, ItemAdapter.ItemViewHolder>(ItemDiffCallback()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ItemViewHolder.from(parent)
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = getItem(position)
+        holder.bind(item)
     }
 
-    class ItemViewHolder(val binding: ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root)
+    class ItemViewHolder(private val binding: ItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Item) {
+            // hace el binding entre el item que se nos pase como parametro y el item
+            // que tenemos en el layout, el cual se encargará de 'pintar' los datos en pantalla
+            binding.item = item
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ItemViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemLayoutBinding.inflate(layoutInflater, parent, false)
+                return ItemViewHolder(binding)
+            }
+        }
+    }
 }
 
-
+// clase de apoyo para que se recalcule la diferencia entre 2 items y hacer el menor
+// esfuerzo para renderizar
 class ItemDiffCallback : DiffUtil.ItemCallback<Item>() {
     override fun areItemsTheSame(oldItem: Item, newItem: Item) = oldItem.id == newItem.id
     override fun areContentsTheSame(oldItem: Item, newItem: Item) = oldItem == newItem
