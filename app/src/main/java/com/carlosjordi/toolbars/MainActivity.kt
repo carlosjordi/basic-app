91059ffc638d50a175888dc97c500b6fe5b52641
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2020-05-04 04:10:46 -0500

    Setea adapter de activity_main
    Se cambia el activity_main layout a databinding
    Se setea el adapter

--- app/src/main/java/com/carlosjordi/toolbars/MainActivity.kt
+++ app/src/main/java/com/carlosjordi/toolbars/MainActivity.kt
@@ -2,11 +2,19 @@ package com.carlosjordi.toolbars
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.carlosjordi.toolbars.databinding.ActivityMainBinding
+import com.carlosjordi.toolbars.toolbar_1.ItemAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val adapter = ItemAdapter()
+
+        binding.itemsList.adapter = adapter
     }
 }
