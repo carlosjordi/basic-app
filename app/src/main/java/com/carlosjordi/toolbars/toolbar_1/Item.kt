9820b06741e12e83b382cf1c85038d482389538a
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2020-05-05 16:56:51 -0500

    Agrega argumento a ItemDetailFragment
    Se implementa parcelable a la clase Item

--- app/src/main/java/com/carlosjordi/toolbars/toolbar_1/Item.kt
+++ app/src/main/java/com/carlosjordi/toolbars/toolbar_1/Item.kt
@@ -1,8 +1,12 @@
 package com.carlosjordi.toolbars.toolbar_1
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Item(
     val id: Long,
     val title: String,
     val description: String,
     val imgResource: Int
-)
+) : Parcelable
